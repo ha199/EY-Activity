9409165c94cf321a951ee63b9ad25fdd1d801a74
Hari Singh Meena <harim4138@gmail.com>
2023-09-13 13:57:40 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,53 +1,28 @@
 
-// 1. Write a Java program that will iterate through all elements in array with int data type atleast 10 elements. (Use any kind of loop)
-
-public class Array {
+public class ArrayOperations {
     public static void main(String[] args) {
         // Define an array of int with at least 10 elements
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // You can add more elements
 
-        // Iterate through the array using a for loop
+        // 1. Iterate through the array using a for loop
+        System.out.println("Iterating through the array:");
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Element at index " + i + ": " + numbers[i]);
         }
-    }
-}
 
-//2. Using the Java program created from item 1, retrieve the first element and last element of the array list.
-
-public class ArrayFirstLastElement {
-    public static void main(String[] args) {
-        // Define an array of int with at least 10 elements
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // You can add more elements
-
-        // Retrieve the first element
+        // 2. Retrieve the first and last element
         int firstElement = numbers[0];
-
-        // Retrieve the last element
         int lastElement = numbers[numbers.length - 1];
-
-        System.out.println("First element: " + firstElement);
+        System.out.println("\nFirst element: " + firstElement);
         System.out.println("Last element: " + lastElement);
-    }
-}
 
-//3. Using the Java program created from item 1, update the 3rd and 8th element of the array.
-public class ArrayUpdateElements {
-    public static void main(String[] args) {
-        // Define an array of int with at least 10 elements
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // You can add more elements
-
-        // Update the 3rd element (index 2)
+        // 3. Update the 3rd element (index 2) and 8th element (index 7)
         numbers[2] = 30;
-
-        // Update the 8th element (index 7)
         numbers[7] = 80;
-
-        // Print the updated array
+        System.out.println("\nUpdated array:");
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Element at index " + i + ": " + numbers[i]);
         }
     }
 }
-
-
+        
